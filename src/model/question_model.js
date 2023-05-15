d0b6ee28c324d4d163508d14826c309205d4b922
smit <monparasmit1@gmail.com>
2023-05-15 13:33:30 +0530

    add cname in Question model

--- src/model/question_model.js
+++ src/model/question_model.js
@@ -1,14 +1,21 @@
 const { Schema, model } = require('mongoose')
+const CategoryModel = require('./category_model')
 
 const Question = new Schema({
     uid: { type: Schema.Types.ObjectId, ref: 'users', required: true },
     cid: { type: Schema.Types.ObjectId, ref: 'categories', required: true },
+    cname: { type: String },
     question: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: [String], default: [] },
     likes: { type: Number, default: 0 },
     userLikes: { type: [Schema.Types.ObjectId], ref: 'users', default: [] },
     createdAt: { type: Date, default: Date.now() }
+})
+
+Question.pre('save', async function (next) {
+    const { name } = await CategoryModel.findById(this.cid)
+    this.cname = name
 })
 
 Question.methods.addLikes = function (uid) {
